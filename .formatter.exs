8dc87ddf9c9ad669ea3620c139d004b4dd643edc
mjheilmann <mjheilmann@@users.noreply.github.com>
2023-11-26 10:50:37 -0500

    fixup formatting config and lint formatting

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,10 @@
-# Used by "mix format"
 [
-  inputs: ["{mix,.formatter,.credo}.exs", "{config,lib,test,examples}/**/*.{ex,exs}"]
+  inputs:
+    [
+      "{mix,.formatter,.credo}.exs",
+      "{config,lib,priv,test}/**/*.{ex,exs}",
+      "examples/helloworld/{config,lib,priv,test}/**/*.{ex,exs}"
+    ]
+    |> Enum.flat_map(&Path.wildcard(&1, match_dot: true))
+    |> Enum.reject(&String.match?(&1, ~r/^.*\.pb\.ex$/))
 ]
