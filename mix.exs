2b09955e4d6e80c201c49adb569e55cf9bd8a3e0
Michael Heilmann <mheilmann@tubi.tv>
2024-06-11 10:10:12 -0400

    add ex_doc dep to enalbe hex.publish

--- mix.exs
+++ mix.exs
@@ -42,9 +42,10 @@ defmodule GrpcReflection.MixProject do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:google_protos, "~> 0.4.0"},
       {:grpc, "~> 0.7"},
-      {:protobuf, "~> 0.11"},
-      {:google_protos, "~> 0.4.0"}
+      {:protobuf, "~> 0.11"}
     ]
   end
 
