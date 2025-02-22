016ff4d739725623cfd304a1749be1ac8e189fa9
mjheilmann <mjheilmann@users.noreply.github.com>
2025-02-21 20:09:54 -0500

    update deps and retest in example app

--- examples/helloworld/mix.exs
+++ examples/helloworld/mix.exs
@@ -18,12 +18,9 @@ defmodule Helloworld.Mixfile do
 
   defp deps do
     [
-      {:grpc, "~> 0.7"},
+      {:grpc, "~> 0.9"},
       {:grpc_reflection, path: "../.."},
-      {:protobuf, "~> 0.11"},
-      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
-      {:google_protos, "~> 0.4.0"},
-      {:protobuf_generate, "~> 0.1.0"}
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false}
     ]
   end
 end
