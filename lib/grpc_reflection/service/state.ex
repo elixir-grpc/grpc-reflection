3fc7b838f86cb2cc9f2e77fe62781b725ed50d43
mjheilmann <mjheilmann@users.noreply.github.com>
2025-06-15 09:02:06 -0400

    cleanup

--- lib/grpc_reflection/service/state.ex
+++ lib/grpc_reflection/service/state.ex
@@ -1,7 +1,7 @@
 defmodule GrpcReflection.Service.State do
   @moduledoc false
 
-  defstruct services: [], files: %{}, symbols: %{}, extensions: %{}, references: MapSet.new()
+  defstruct services: [], files: %{}, symbols: %{}, extensions: %{}
 
   @type descriptor_t :: GrpcReflection.Server.descriptor_t()
   @type entry_t :: %{optional(binary()) => descriptor_t()}
@@ -10,8 +10,7 @@ defmodule GrpcReflection.Service.State do
           services: list(module()),
           files: entry_t(),
           symbols: entry_t(),
-          extensions: %{optional(binary()) => list(integer())},
-          references: MapSet.t(binary())
+          extensions: %{optional(binary()) => list(integer())}
         }
 
   @spec new(list(module)) :: t()
@@ -23,8 +22,7 @@ defmodule GrpcReflection.Service.State do
       services: Enum.uniq(state1.services ++ state2.services),
       files: Map.merge(state1.files, state2.files),
       symbols: Map.merge(state1.symbols, state2.symbols),
-      extensions: Map.merge(state1.extensions, state2.extensions),
-      references: MapSet.union(state1.references, state2.references)
+      extensions: Map.merge(state1.extensions, state2.extensions)
     }
   end
 
@@ -41,13 +39,6 @@ defmodule GrpcReflection.Service.State do
   def add_extensions(%__MODULE__{} = state, extensions) do
     %{state | extensions: Map.merge(extensions, state.extensions)}
   end
-
-  def add_references(%__MODULE__{} = state, refs) do
-    references = Enum.reduce(refs, state.references, &MapSet.put(&2, &1))
-    %{state | references: references}
-  end
-
-  def get_references(%__MODULE__{} = state), do: MapSet.to_list(state.references)
 
   def lookup_services(%__MODULE__{services: services}) do
     Enum.map(services, fn service_mod -> service_mod.__meta__(:name) end)
@@ -72,15 +63,6 @@ defmodule GrpcReflection.Service.State do
 
   def has_symbol?(state, symbol) do
     Map.has_key?(state.symbols, symbol)
-  end
-
-  @doc """
-  Get the list of refereneces that are not known symbols
-  """
-  def get_missing_references(%__MODULE__{} = state) do
-    state.references
-    |> MapSet.to_list()
-    |> Enum.reject(&Map.has_key?(state.symbols, &1))
   end
 
   def lookup_filename(filename, %__MODULE__{files: files}) do
