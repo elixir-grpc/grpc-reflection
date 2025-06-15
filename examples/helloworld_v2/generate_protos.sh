b81d0ebefe9787dd79c24eb313cef83ae7f40c5a
mjheilmann <mjheilmann@users.noreply.github.com>
2025-06-15 09:22:05 -0400

    remove double-line in examples v2 generate_protos

--- examples/helloworld_v2/generate_protos.sh
+++ examples/helloworld_v2/generate_protos.sh
@@ -3,7 +3,6 @@
 protoc \
   -I deps/protobuf/src \
   -I priv/protos \
-  --elixir_out=plugins=grpc:./lib/protos \
   --elixir_opt=include_docs=true \
   --elixir_out=plugins=grpc,gen_descriptors=true:lib/protos \
   priv/protos/*.proto
